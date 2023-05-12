6136dd1e0e557a8da1983c74608f609b6b8f2fc3
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-05-11 18:02:19 -0700

    Use blocks

--- analytics.py
+++ analytics.py
@@ -2,52 +2,60 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
+import time
+from concurrent.futures import ThreadPoolExecutor
 
 load_dotenv()
 
-def get_mint_events(base_url, contract_address, api_key, topic0):
-    page = 1
-    offset = 1000
+def get_mint_events(network):
+    base_url = network['url']
+    contract_address = os.getenv(network['contract_address_env'])
+    api_key = os.getenv(network['api_key_env'])
+    topic0 = "0x7650948236619e679e44bf502d527ec950d1d58336e6babf229f483c57d04672"
+
+    block_step = 5000
+    from_block = 16633100
+    to_block = from_block + block_step
+    max_block = 45000000  # Set this to the maximum block number you want to search up to
     total_logs = 0
 
-    while True:
-        # Get logs of Mint function calls
+    while from_block <= max_block:
         response = requests.get(base_url, params={
             "module": "logs",
             "action": "getLogs",
-            "fromBlock": "0",
-            "toBlock": "latest",
+            "fromBlock": str(from_block),
+            "toBlock": str(to_block),
             "address": contract_address,
             "topic0": topic0,
-            "page": str(page),
-            "offset": str(offset),
+            "offset": "1000",
             "apikey": api_key
         })
 
-        # Check if the response is not empty and status code is 200
+        print(f"Requested from block {from_block} to block {to_block}. Response status: {response.status_code}")
+
         if response.status_code == 200 and response.text.strip():
-            logs = response.json().get('result')
+            response_json = response.json()
+            print(f"Response: {response_json}")
+            logs = response_json.get('result')
             if isinstance(logs, list):
                 num_logs = len(logs)
 
-                if num_logs == 0:
-                    # If no more logs, stop requesting
-                    break
-
                 total_logs += num_logs
             else:
-                print(f"Error: 'result' not found or not a list in response from {base_url}")
-                break
+                print(f"Error: 'result' not found or not a list in response from {base_url}. Full response: {response_json}")
         else:
-            # Add error handling here. For example, print an error message and break the loop
             print(f"Error: Received invalid response from {base_url}")
-            break
 
-        page += 1
+        from_block = to_block + 1
+        to_block = min(to_block + block_step, max_block)
+        time.sleep(14)
 
+    print(f"Number of mint events for {network}: {total_logs}")
     return total_logs
 
-# Define network data
+# Rest of your code...
+
+
 networks = {
     "etherscan": {
         "url": "https://api.etherscan.io/api",
@@ -71,16 +79,8 @@ networks = {
     }
 }
 
-topic0 = "0x7650948236619e679e44bf502d527ec950d1d58336e6babf229f483c57d04672"  # Updated topic0
-
-total_mint_events = 0
-
-for network in networks.values():
-    base_url = network['url']
-    contract_address = os.getenv(network['contract_address_env'])
-    api_key = os.getenv(network['api_key_env'])
-    num_mint_events = get_mint_events(base_url, contract_address, api_key, topic0)
-    total_mint_events += num_mint_events
-    print(f"Number of mint events for {network}: {num_mint_events}")
+with ThreadPoolExecutor() as executor:
+    mint_event_futures = {executor.submit(get_mint_events, network): network for network in networks.values()}
+    total_mint_events = sum(future.result() for future in mint_event_futures)
 
 print(f"Total number of mint events: {total_mint_events}")
