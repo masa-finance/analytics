31863c110715bcab99e7959e6ed1bb6319046eb6
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-05-18 22:48:08 -0700

    add event check

--- src/batch/batch.py
+++ src/batch/batch.py
@@ -10,13 +10,13 @@ load_dotenv()
 def check_and_create_user(address, base_url):
     users_url = f"{base_url}/users/{address}"
     user_exists = requests.get(users_url)
-    if user_exists.status_code == 404:  # Assuming a 404 status code means the user does not exist
+    if user_exists.status_code == 404: # Assuming a 404 status code means the user does not exist
         user_creation_response = requests.post(
             f"{base_url}/users",
             headers={"Content-Type": "application/json"},
             data=json.dumps({"address": address}),
         )
-        if user_creation_response.status_code != 201:  # Adjusted to check for 201 status code
+        if user_creation_response.status_code != 201: # Adjusted to check for 201 status code
             print(
                 f"Failed to create user {address}. Status code: {user_creation_response.status_code}. Response: {user_creation_response.text}"
             )
@@ -27,27 +27,35 @@ def check_and_create_user(address, base_url):
             f"Error checking if user {address} exists. Status code: {user_exists.status_code}. Response: {user_exists.text}"
         )
 
+def check_event_exists(address, base_url):
+    events_url = f"{base_url}/events/by-address/{address}"
+    response = requests.get(events_url)
+    if response.status_code == 200 and response.json():
+        return True
+    return False
+
 def add_event_to_user(address, base_url, timestamp, network_name):
-    event_creation_response = requests.post(
-        f"{base_url}/events",
-        headers={"Content-Type": "application/json"},
-        data=json.dumps(
-            {
-                "type": "Mint: MGX-2FA",
-                "user_address": address,
-                "event_data": {
-                    "network": network_name,
-                    "date": datetime.datetime.utcfromtimestamp(timestamp).isoformat()
-                },
-            }
-        ),
-    )
-    if event_creation_response.status_code != 201:  # Adjusted to check for 201 status code
-        print(
-            f"Failed to add event to user {address}. Status code: {event_creation_response.status_code}. Response: {event_creation_response.text}"
+    if not check_event_exists(address, base_url):
+        event_creation_response = requests.post(
+            f"{base_url}/events",
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(
+                {
+                    "type": "Mint: MGX-2FA",
+                    "user_address": address,
+                    "event_data": {
+                        "network": network_name,
+                        "date": datetime.datetime.utcfromtimestamp(timestamp).isoformat()
+                    },
+                }
+            ),
         )
-    else:
-        print(f"Successfully added event to user {address}")
+        if event_creation_response.status_code != 201: # Adjusted to check for 201 status code
+            print(
+                f"Failed to add event to user {address}. Status code: {event_creation_response.status_code}. Response: {event_creation_response.text}"
+            )
+        else:
+            print(f"Successfully added event to user {address}")
 
 def get_mint_events(network, network_name):
     base_url = network['url']
@@ -56,14 +64,14 @@ def get_mint_events(network, network_name):
     topic0 = "0x7650948236619e679e44bf502d527ec950d1d58336e6babf229f483c57d04672"
 
     block_step = 5000
-    from_block = network['start_block']  
+    from_block = network['start_block']
     to_block = from_block + block_step
-    max_block = 45000000  
+    max_block = 45000000
     total_logs = 0
 
     while from_block <= max_block:
         page = 1
-        while True:  # Loop to handle pagination
+        while True: # Loop to handle pagination
             response = requests.get(base_url, params={
                 "module": "logs",
                 "action": "getLogs",
@@ -85,27 +93,27 @@ def get_mint_events(network, network_name):
                 if isinstance(logs, list):
                     num_logs = len(logs)
                     total_logs += num_logs
-                    if num_logs == 0:  # No more logs, break the pagination loop
+                    if num_logs == 0: # No more logs, break the pagination loop
                         break
                     for log in logs:
                         to_address = '0x' + log['data'][90:130]
-                        timestamp = int(log['timeStamp'], 16)  
+                        timestamp = int(log['timeStamp'], 16)
 
                         check_and_create_user(to_address, "https://zksbt-cookie-api.onrender.com")
                         add_event_to_user(to_address, "https://zksbt-cookie-api.onrender.com", timestamp, network_name)
                 else:
                     print(f"Error: 'result' not found or not a list in response from {base_url}. Full response: {response_json}")
-                    break  # Break the pagination loop in case of error
+                    break # Break the pagination loop in case of error
             else:
-                print(f"Error: Received invalid response from {base_url}")
-                break  # Break the pagination loop in case of error
+                print(f"Error: Response status not 200 or empty response text. Full response: {response.text}")
+                break # Break the pagination loop in case of error
 
-            page += 1  # Increase page number for the next iteration
+            page += 1
 
-        from_block = to_block + 1
-        to_block = min(to_block + block_step, max_block)
-        time.sleep(10)
+        from_block += block_step
+        if to_block + block_step > max_block:
+            to_block = max_block
+        else:
+            to_block += block_step
 
-    print(f"Number of mint events for {network_name}: {total_logs}")
-    return total_logs
-
+    print(f"Total logs found: {total_logs}")
